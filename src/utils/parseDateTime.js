940540f137544da7e63db855c995ed476f146880
puppycat710 <puppycat710@proton.me>
2025-10-07 11:44:17 -0300

    kkk

--- src/utils/parseDateTime.js
+++ src/utils/parseDateTime.js
@@ -1,7 +1,7 @@
 export default function parseDateTime(str) {
-  const [datePart, timePart] = str.split(" ")
-  const [year, month, day] = datePart.split("-").map(Number)
-  const [hours, minutes, seconds] = timePart.split(":").map(Number)
-  // const utcTime = Date.UTC(year, month - 1, day, hours, minutes, seconds)
-  return new Date(year, month - 1, day, hours, minutes, seconds)
+	const [datePart, timePart] = str.split(' ')
+	const [year, month, day] = datePart.split('-').map(Number)
+	const [hours, minutes, seconds] = timePart.split(':').map(Number)
+	const utcTime = Date.UTC(year, month - 1, day, hours, minutes, seconds)
+	return new Date(utcTime)
 }
