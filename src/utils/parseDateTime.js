246d40d62ed9423f64f416199845cd5af6d383fb
puppycat710 <puppycat710@proton.me>
2025-10-07 13:25:48 -0300

    kkk3

--- src/utils/parseDateTime.js
+++ src/utils/parseDateTime.js
@@ -2,7 +2,9 @@ export default function parseDateTime(str) {
 	const [datePart, timePart] = str.split(' ')
 	const [year, month, day] = datePart.split('-').map(Number)
 	const [hours, minutes, seconds] = timePart.split(':').map(Number)
+	// O horário no banco é de Brasília (UTC-3)
+	// Então adicionamos +3h para converter para UTC (servidor)
+	const utcTime = Date.UTC(year, month - 1, day, hours + 3, minutes, seconds)
 
-	// Cria data local (sem UTC)
-	return new Date(year, month - 1, day, hours, minutes, seconds)
+	return new Date(utcTime)
 }
