3b7a3fdd44c61ee444e8192faa779d75e06a6656
puppycat710 <puppycat710@proton.me>
2025-10-09 15:03:27 -0300

    HEXA

--- src/utils/sendMessageToGroup.js
+++ src/utils/sendMessageToGroup.js
@@ -4,17 +4,14 @@ import messageRepository from '../repositories/message/message.repository.js'
 import logMessage from './logMessage.js'
 
 export default async function sendMessageToGroup(msg) {
-	console.log('>>>>>>>>>1')
 	const contacts = await contactGroupRepository.getContactsByGroupId(msg.fk_group_id)
-	console.log(contacts)
+
 	for (const contact of contacts) {
 		try {
-			console.log('>>>>>>>>>2')
 			const res = await api.post('/send', {
 				number: contact.contact,
 				message: msg.text,
 			})
-			console.log('>>>>>>>>>3')
 			logMessage(msg, contact)
 			console.log(res)
 		} catch (err) {
