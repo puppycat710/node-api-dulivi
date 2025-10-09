7cba045b6b982353dc26b757af8ffb2424a93fba
puppycat710 <puppycat710@proton.me>
2025-10-09 10:05:21 -0300

    HEXA vem 2

--- src/utils/parseDateTime.js
+++ src/utils/parseDateTime.js
@@ -4,7 +4,7 @@ export default function parseDateTime(str) {
 	const [hours, minutes, seconds] = timePart.split(':').map(Number)
 	// O horário no banco é de Brasília (UTC-3)
 	// Então adicionamos +3h para converter para UTC (servidor)
-	const utcTime = Date.UTC(year, month - 1, day, hours + 3, minutes, seconds)
+	const utcTime = Date.UTC(year, month - 1, day, hours - 1, minutes, seconds)
 
 	return new Date(utcTime)
 }
