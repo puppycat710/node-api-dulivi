d01e0a68258ce5aeee8b4040f7314f4f2cc7b478
puppycat710 <puppycat710@proton.me>
2025-10-11 22:35:05 -0300

    slug store

--- src/utils/createSlug.js
+++ src/utils/createSlug.js
@@ -1,6 +1,6 @@
 /**
  * Converte um nome de restaurante em uma slug amigável para URLs.
- * Exemplo: "Nabih Esfiha's" => "nabihesfihas"
+ * Exemplo: "Nabih Esfiha's" => "nabih-esfihas"
  *
  * @param {string} name - Nome original do restaurante
  * @returns {string} - Slug formatada
@@ -8,7 +8,9 @@
 export const createSlug = (name) => {
 	return name
 		.normalize('NFD') // Separa acentos das letras
-		.replace(/[\u0300-\u036f]/g, '') // Remove os acentos
-		.replace(/[^a-zA-Z0-9]/g, '') // Remove caracteres especiais e espaços
+		.replace(/[\u0300-\u036f]/g, '') // Remove acentos
+		.replace(/[^a-zA-Z0-9\s]/g, '') // Remove caracteres especiais, mas mantém espaços
+		.trim() // Remove espaços extras no início e no fim
+		.replace(/\s+/g, '-') // Substitui espaços por hífens
 		.toLowerCase() // Converte para minúsculas
 }
