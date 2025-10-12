835fc38c5074fe46004a28364e7f85e9f88ead2c
puppycat710 <puppycat710@proton.me>
2025-10-11 23:22:36 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -7,24 +7,19 @@ import routes from './routes/index.js'
 import './jobs/scheduler.js'
 
 export const corsOptions = {
-	origin: [
-		// Domínios permitidos
-		'https://dulivii.netlify.app', // Cardápio Digital production
-		'https://cardapio-digital-api-nzm1.onrender.com', // API production
-		'https://node-api-dulivi-whatsapp-bot-production.up.railway.app/', // API production
-		'http://localhost:3000', // API development
-		'http://localhost:5173', // Cardápio Digital development
-	],
+	origin: urls,
 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
 	allowedHeaders: ['Content-Type', 'Authorization'],
 	credentials: true,
 }
 const urls = [
 	// Domínios permitidos
-	'https://dulivi.netlify.app', // Cardápio Digital production
+	'https://menu-dulivi.netlify.app/', // Cardápio production
+	'https://painel-dulivi.netlify.app/', // Painel production
 	'https://cardapio-digital-api-nzm1.onrender.com', // API production
+	'https://node-api-dulivi-whatsapp-bot-production.up.railway.app/', // API production
 	'http://localhost:3000', // API development
-	'http://localhost:5173', // Cardápio Digital development
+	'http://localhost:5173', // Dulivi development
 ]
 const app = express()
 
