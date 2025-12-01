b697cef1f49049ff106e3b08bcc6eb06b892675b
puppycat710 <puppycat710@proton.me>
2025-11-30 23:38:38 -0300

    DULIVI CARALHO

--- src/app.js
+++ src/app.js
@@ -12,6 +12,8 @@ const urls = [
 	'https://painel-dulivi.netlify.app', // Painel production
 	'https://cardapio-digital-api-nzm1.onrender.com', // API production
 	'https://node-api-dulivi-whatsapp-bot-production.up.railway.app', // API production
+	'https://lp-dulivi.netlify.app/', // LP
+	'http://localhost:8080', // LP development
 	'http://localhost:3000', // API development
 	'http://localhost:5173', // Dulivi development
 ]
